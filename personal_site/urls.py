fad6c31b38dc342a364551347f4ae694983de385
Shailesh Rudra <rudra12shailesh@gmail.com>
2017-08-27 15:30:26 +0530

    Files reformatted

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
+
 from blog import views
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^froala_editor/', include('froala_editor.urls')),
-    url(r'^blog/',include('blog.urls',namespace='blog',app_name='blog')),
-    
-    
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
 ]
